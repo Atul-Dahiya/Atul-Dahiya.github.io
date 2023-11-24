a9f23f7180b66893ff105912a41ba58109cbc81e
Atul-Dahiya <atul.dahiya4@icloud.com>
2023-11-24 17:57:45 -0500

    Fifth commit

--- Assignment4/Part 4/main-finished.js
+++ Assignment4/Part 4/main-finished.js
@@ -69,38 +69,61 @@ class Ball extends Shape {
    }
  }
 }
-   
 
-
-const balls = [];
-
-while (balls.length < 25) {
-   const size = random(10,20);
-   const ball = new Ball(
-      // ball position always drawn at least one ball width
-      // away from the edge of the canvas, to avoid drawing errors
-      random(0 + size,width - size),
-      random(0 + size,height - size),
-      random(-7,7),
-      random(-7,7),
-      randomRGB(),
-      size
-   );
-
-  balls.push(ball);
-}
-
-function loop() {
-   ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
-   ctx.fillRect(0, 0,  width, height);
-
-   for (const ball of balls) {
-     ball.draw();
-     ball.update();
-     ball.collisionDetect();
+class EvilCircle extends Shape {
+   constructor(x, y) {
+     super(x, y, 20, 20);
+     this.color = 'white';
+     this.size = 10;
+   }
+ 
+   draw() {
+     ctx.beginPath();
+     ctx.strokeStyle = this.color;
+     ctx.lineWidth = 3;
+     ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+     ctx.stroke();
+   }
+ 
+   checkBounds() {
+     if (this.x + this.size >= width || this.x - this.size <= 0) {
+       this.velX = -this.velX;
+     }
+ 
+     if (this.y + this.size >= height || this.y - this.size <= 0) {
+       this.velY = -this.velY;
+     }
    }
 
-   requestAnimationFrame(loop);
-}
+   collisionDetect() {
+      for (const ball of balls) {
+        if (ball.exists) {
+          const dx = this.x - ball.x;
+          const dy = this.y - ball.y;
+          const distance = Math.sqrt(dx * dx + dy * dy);
+  
+          if (distance < this.size + ball.size) {
+            ball.exists = false;
+            count--;
+            para.textContent = 'Ball count: ' + count;
+          }
+        }
+      }
+    }
+  }
 
-loop();
+  const balls = Array.from({ length: 25 }, () => {
+   const size = random(10, 20);
+   const ball = new Ball(
+     random(size, width - size),
+     random(size, height - size),
+     random(-7, 7),
+     random(-7, 7),
+     randomRGB(),
+     size
+   );
+   count++;
+   para.textContent = 'Ball count: ' + count;
+   return ball;
+ });
+ 
